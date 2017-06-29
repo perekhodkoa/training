698b69dd1df1c7a8967f50aabdce7064acf0fc30
perekhodkoa <anatolii.perekhodko@aol.com>
2017-06-28 22:38:28 -0400

    added some stuff

--- src/training/com/BinarySearch/IOStuff/IOThings.java
+++
@@ -1,7 +0,0 @@
-package training.com.BinarySearch.IOStuff;
-
-/**
- * Created by sentinel on 6/28/17.
- */
-public class IOThings {
-}
---
+++ src/training/com/IOStuff/IOThings.java
@@ -0,0 +1,25 @@
+package training.com.BinarySearch.IOStuff;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Created by sentinel on 6/28/17.
+ */
+public class IOThings {
+
+    private void ReadLineByLine(String file) throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                // process the line.
+            }
+        } catch (IOException ex) {
+
+        } catch (Exception ex) {
+
+        }
+    }
+
+}
